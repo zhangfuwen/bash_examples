7300d7045ffc3834d7e07fdb41cbcb233bb8b067
zhangfuwen <zhangfuwen@bytedance.com>
2021-04-09 15:21:17 +0800

    update yes_no.sh

--- yes_no.sh
+++ yes_no.sh
@@ -1,8 +1,18 @@
-while true; do
-    read -p "Do you wish to install this program?[Yy/Nn]" yn
-    case $yn in
-        [Yy]* ) echo "you said yes"; break;;
-        [Nn]* ) echo "you said no"; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+function user_confirm()
+{
+    local msg=$1
+    while true; do
+        read -p "$msg[Yy/Nn]" yn
+        case $yn in
+            [Yy]* ) echo 0; break;;
+            [Nn]* ) echo 1; break;;
+            * ) echo "Please answer yes[Yy] or no[Nn].";;
+        esac
+    done
+}
+
+if [[ $(user_confirm "Do you want to do this?") ]]; then
+    echo "you said yes"
+else
+    echo "you said no"
+fi
